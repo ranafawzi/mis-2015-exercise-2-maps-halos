cbb11061b515f76685e49a79a8eecb0a14dc68cc
ranafawzi <ranafawzi85@yahoo.com>
2015-07-01 11:59:30 +0200

    Finished code upload

--- HelloMaps/app/src/main/java/com/example/mmbuw/hellomaps/MapsActivity.java
+++ HelloMaps/app/src/main/java/com/example/mmbuw/hellomaps/MapsActivity.java
@@ -1,28 +1,92 @@
 package com.example.mmbuw.hellomaps;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity {
+import java.util.HashSet;
+import java.util.Set;
+
+public class MapsActivity extends Activity implements GoogleMap.OnMapLongClickListener {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+
+    EditText message;
+
+    Set locationMessage = new HashSet();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        message = (EditText) findViewById(R.id.message);
+
+        Context context = getApplicationContext();
+        CharSequence text = "Application Started";
+        int duration = Toast.LENGTH_LONG;
+
+        Toast toast = Toast.makeText(context,text,duration);
+        toast.show();
+
         setUpMapIfNeeded();
+
+        mMap.setMyLocationEnabled(true);
+
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener(){
+
+            @Override
+            public void onMapLongClick(LatLng latLng){
+                mMap.addMarker(new MarkerOptions().position(latLng).title(message.getText().toString()));
+
+                locationMessage.add(message.getText().toString());
+
+                SharedPreferences prefs = getSharedPreferences("Weimar",Activity.MODE_PRIVATE);
+
+                SharedPreferences.Editor editor = prefs.edit();
+
+                editor.putStringSet("message",locationMessage);
+
+                Boolean flag = editor.commit();
+
+                if(flag == true){
+
+                    Context context = getApplicationContext();
+                    CharSequence text = "Saved at Shared Preference";
+                    int duration = Toast.LENGTH_LONG;
+
+                    Toast toast = Toast.makeText(context,text,duration);
+                    toast.show();
+                }
+
+                CircleOptions circle = new CircleOptions();
+                circle.center(latLng).fillColor(0x40ff0000).radius(10000).strokeColor(Color.GREEN).strokeWidth(3);
+                mMap.addCircle(circle);
+            }
+        });
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         setUpMapIfNeeded();
+    }
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+
     }
 
     /**
@@ -44,7 +108,7 @@ public class MapsActivity extends FragmentActivity {
         // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
             // Try to obtain the map from the SupportMapFragment.
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+            mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
                     .getMap();
             // Check if we were successful in obtaining the map.
             if (mMap != null) {
